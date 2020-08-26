e3243e04b64f800ed20eb468dd68e43481e0ab17
Maitland Marshall <maitlandmarshallapps@gmail.com>
2020-08-26 13:09:40 +1000

    JobFactory

---
+++ MAD.Integration.Common/Jobs/JobFactory.cs
@@ -0,0 +1,29 @@
+﻿using Hangfire;
+using Hangfire.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MAD.Integration.Common.Jobs
+{
+    public static class JobFactory
+    {
+        public static void CreateRecurringJob<T>(string jobName, Expression<Func<T, Task>> methodCall)
+        {
+            IStorageConnection connection = JobStorage.Current.GetConnection();
+
+            RecurringJob.AddOrUpdate<T>(
+                recurringJobId: jobName,
+                methodCall: methodCall,
+                cronExpression: Cron.Daily(22, 30),
+                timeZone: TimeZoneInfo.Local);
+
+            RecurringJobDto newJob = connection.GetRecurringJobs(new string[] { jobName }).First();
+
+            if (newJob.LastExecution is null)
+                RecurringJob.Trigger(jobName);
+        }
+    }
+}
