06e7cf05a3508aef4ec0df61f7ab52d73c1b784a
maitlandmarshall <maitlandmarshallapps@gmail.com>
2022-12-01 12:10:48 +1000

    Add ICreateDestination and implement default sql server create destination for creating tables automatically

---
+++ MIFCore.Hangfire.APIETL.SqlServer/DefaultSqlServerCreateDestination.cs
@@ -0,0 +1,41 @@
+﻿using ETLBox.Connection;
+using ETLBox.ControlFlow;
+using ETLBox.ControlFlow.Tasks;
+using MIFCore.Hangfire.APIETL.Load;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIFCore.Hangfire.APIETL.SqlServer
+{
+    internal class DefaultSqlServerCreateDestination : ICreateDestination
+    {
+        private readonly HangfireConfig hangfireConfig;
+
+        public DefaultSqlServerCreateDestination(HangfireConfig hangfireConfig)
+        {
+            this.hangfireConfig = hangfireConfig;
+        }
+
+        public async Task OnCreateDestination(CreateDestinationArgs args)
+        {
+            var apiEndpointModel = args.ApiEndpointModel;
+            var connMan = new SqlConnectionManager(this.hangfireConfig.ConnectionString);
+
+            if (IfTableOrViewExistsTask.IsExisting(connMan, apiEndpointModel.DestinationName) == false)
+            {
+                var tableDefinition = new TableDefinition(
+                    name: apiEndpointModel.DestinationName,
+                    columns: apiEndpointModel.MappedProperties.Select(kvp =>
+                    {
+                        var (key, value) = kvp;
+                        return new TableColumn(value.DestinationName, value.DestinationType, value.IsKey == false, value.IsKey, false);
+                    }).ToList());
+
+                await Task.Run(() =>
+                {
+                    CreateTableTask.Create(connMan, tableDefinition);
+                });
+            }
+        }
+    }
+}
---
+++ MIFCore.Hangfire.APIETL/Load/CreateDestinationArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace MIFCore.Hangfire.APIETL.Load
+{
+    public class CreateDestinationArgs
+    {
+        public CreateDestinationArgs(ApiEndpointModel apiEndpointModel)
+        {
+            this.ApiEndpointModel = apiEndpointModel;
+        }
+
+        public ApiEndpointModel ApiEndpointModel { get; }
+    }
+}
